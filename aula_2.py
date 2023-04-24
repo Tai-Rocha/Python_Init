86cfd3a509961d8bfd0b187b1cfc421e495f9a80
Tai-Rocha <taina013@gmail.com>
2023-04-24 18:51:14 -0300

    add aula_2 (via Rstudio)

---
+++ aula_2.py
@@ -0,0 +1,13 @@
+# Aula 2 - Mulheres em dados 
+# O objetivo de controlar um fluxo pode variar bastante:
+
+#"se a temperatura estiver acima de 30 graus Celsius, ligue o ar condicionado"
+#"enquanto a temperatura estiver acima de 25 graus Celsius, ligue o ventilador"
+#"para cada item na lista de compras, adicione o item ao carrinho de compras"
+# E como seria no Python?
+
+
+#"se a temperatura estiver acima de 30 graus Celsius, ligue o ar condicionado"
+x = 31
+if x > 30:
+  print("Ligue o ar condicionado")
