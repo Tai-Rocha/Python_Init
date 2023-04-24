cea12b2174e962f29f44d989f686d968a69e30a8
Tai-Rocha <taina013@gmail.com>
2023-04-24 19:09:33 -0300

    aula_2 add py script

--- aula_2.py
+++ aula_2.py
@@ -7,7 +7,26 @@
 # E como seria no Python?
 
 
-#"se a temperatura estiver acima de 30 graus Celsius, ligue o ar condicionado"
+# se a temperatura estiver acima de 30 graus Celsius, ligue o ar condicionado"
 x = 31
 if x > 30:
   print("Ligue o ar condicionado")
+
+#se a temperatura não estiver acima de 30 graus Celsius, não ligue o ar condicionado"
+x = 29
+if x > 30:
+  print("Ligue o ar condicionado")
+else:
+  print("Não ligue o ar condicionado")
+  
+  
+#se a temperatura estiver entre 25 e 30 graus Celsius, ligue o ventilador"
+x = int(input("Digite a temperatura: "))
+if 25 <= x <= 30:
+    print("Ligue o ventilador")
+elif x > 30:
+    print("Ligue o ar condicionado")
+else:
+  print("Não ligue o ar condicionado")
+  
+  
